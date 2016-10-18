e6e2cd785b42c857c82a6bb45533f1385fee755d
Kimitoshi Takahshi <ktaka@ccmp.jp>
2016-10-18 19:56:59 +0900

    IP Address change

--- files/hosts_gen.sh
+++ files/hosts_gen.sh
@@ -6,8 +6,8 @@ hosts=/etc/hosts
 touch ./hosts.tmp
 
 for i in {1..250} ; do 
-	if ! egrep "172.16.1.$i"  $hosts > /dev/null ; then
-		echo -e "172.16.1.$i\tv$(printf %03d $i)" >> ./hosts.tmp
+	if ! egrep "10.0.$i.1"  $hosts > /dev/null ; then
+		echo -e "10.0.$i.1\tv$(printf %03d $i)" >> ./hosts.tmp
 	fi
 done  
 
