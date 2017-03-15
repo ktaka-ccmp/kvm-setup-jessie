e71bf00a1231b0df7d7eb8c48bac6c6b97ab6db1
Automatic Web updater <ktaka@ccmp.jp>
2017-03-16 06:29:53 +0900

    10.0.0.x/24 for VMs

--- files/hosts_gen.sh
+++ files/hosts_gen.sh
@@ -5,10 +5,10 @@ hosts=/etc/hosts
 
 touch ./hosts.tmp
 
-for j in {1..3} ; do 
+for j in 0 ; do 
 for i in {1..250} ; do 
-	if ! egrep "172.16.$j.$i"  $hosts > /dev/null ; then
-		echo -e "172.16.$j.$i\tv$(printf %03d $i).k0$j" >> ./hosts.tmp
+	if ! egrep "10.0.$j.$i"  $hosts > /dev/null ; then
+		echo -e "10.0.$j.$i\tv$(printf %03d $i)" >> ./hosts.tmp
 	fi
 done  
 done
